31134d68be705b07158974e1ecf3147a07817c58
Aamir <aamirasaram2107@gmail.com>
2023-07-03 17:28:41 +0200

    Change Kanye Rest API to API Ninja

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -6,26 +6,42 @@ function Quote() {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    try {
+    const fetchQuote = async () => {
       setLoading(true);
-      fetch('https://api.kanye.rest/')
-        .then((response) => response.json())
-        .then((data) => {
-          setQuote(data.quote);
-          setLoading(false);
+        const url = 'https://api.api-ninjas.com/v1/quotes?category=alone';
+        const response = await fetch(url, {
+          headers: {
+            'X-Api-Key': 'ISRtUiJK4f8rcKFOdZaygB2hrj5CGAoYJzY7VKRu',
+          },
         });
-    } catch (err) {
-      setLoading(false);
-      setError(true);
+        if (!response.ok) throw new Error('Request failed');
+        const data = await response.json();
+        setQuote(data[0].quote);
+        setLoading(false);
     }
+    fetchQuote();
+
   }, []);
 
-  if (loading) { return <div className="quote">Loading...</div>; }
-  if (error) { return <div className="quote">Error</div>; }
+  if (loading) {
+    return (
+      <div className="quote-container">
+        <div className="quote">Loading...</div>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className="quote-container">
+        <div className="quote">Error</div>
+      </div>
+    )
+  }
 
   return (
-    <div className="quote">
-      <p>{quote}</p>
+    <div className="quote-container">
+      <div className="quote">{quote}</div>
     </div>
   );
 }
