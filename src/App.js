67db0097178002d7cebd07720cf745fbc846cb62
Aamir <aamirasaram2107@gmail.com>
2023-07-10 16:32:19 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <>
+      <Navigation />
       <Calculator />
       <Quote />
     </>
