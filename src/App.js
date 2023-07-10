d9addd6c0c8261cfb9aa9c885e58dafb61fa2402
Aamir <aamirasaram2107@gmail.com>
2023-07-10 16:55:04 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import Navigation from './components/Navigation';
+import { Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navigation />
-      <Calculator />
-      <Quote />
+      <main>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,37 @@
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const links = [
-  { path: '/', text: 'Home' },
-  { path: '/calculate', text: 'Calculate' },
-  { path: '/quote', text: 'Quote' },
+const pages = [
+  {
+    id: 'home',
+    path: '/',
+    name: 'Home',
+  },
+  {
+    id: 'calculator',
+    path: '/calculator',
+    name: 'Calculator',
+  },
+  {
+    id: 'quote',
+    path: '/quote',
+    name: 'Quote',
+  },
 ];
-const Navbar = () => (
-  <nav className="navbar">
-    <h1>Math Magicians</h1>
-    <ul className="navigation">
-      {links.map((link) => (
-        <li key={link.text}>
-          <NavLink to={link.path}>{link.text}</NavLink>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
-export default Navbar;
+
+const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <h1>Math Magicians</h1>
+      <ul>
+        {pages.map((page) => (
+          <li key={page.id}>
+            <Link to={page.id}>{page.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
